ff8f4c5412ce0ff7b9469691e6a2f28cd75d6f8c
mac <mac@shichenzhi.local>
2018-04-03 19:49:02 +0800

    Initial Commit

---
+++ AdjustLabelText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AdjustLabelText
+//
+//  Created by shichenzhi on 2018/4/3.
+//  Copyright © 2018年 sy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
